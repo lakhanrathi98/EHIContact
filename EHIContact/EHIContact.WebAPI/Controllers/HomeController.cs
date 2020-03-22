45384fcab7b6b0526de2c2634c8e31d8a80c3eb0
Lakhan Rathi <lakhantrathi98@gmail.com>
2020-03-22 22:34:24 +0530

    Implemented basic Single Page Application using jQuery, bootstrap and DataTable plugin which calls contact Web APIs

--- EHIContact/EHIContact.WebAPI/Controllers/HomeController.cs
+++ EHIContact/EHIContact.WebAPI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EHIContact.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,7 @@ namespace EHIContact.WebAPI.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View(new Contact());
         }
     }
 }
